db3649d7851efb9e0057f755b9c980a8441d58d6
ggneilc <nchurro25@proton.me>
2026-02-14 19:06:33 -0600

    workouts updated to scale efficiently

--- workouts/admin.py
+++ workouts/admin.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Workout, Lift, WorkoutType, Set
+from .models import Workout, Lift, WorkoutType, Set, WeeklyVolume, MovementLibrary, Movement, WorkoutTypeBodypart
 
 admin.site.register(Workout)
 admin.site.register(WorkoutType)
+admin.site.register(WorkoutTypeBodypart)
 admin.site.register(Lift)
 admin.site.register(Set)
+admin.site.register(Movement)
+admin.site.register(MovementLibrary)
+admin.site.register(WeeklyVolume)
